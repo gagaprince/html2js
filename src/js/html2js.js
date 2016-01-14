435829ef6d8a13a539e6e4c88c46123b81b28f05
zidong.wang <zidong.wang@qunar.com>
2016-01-14 20:27:59 +0800

    add html2js

---
+++ src/js/html2js.js
@@ -0,0 +1,61 @@
+(function(win){
+    function Html2js(){}
+    Html2js.prototype={
+        init:function(){
+            this.initPage();
+            this.initListener();
+        },
+        initPage:function(){
+            $("#in").val("");
+            $("#out").val("");
+        },
+        initListener:function(){
+            var _this = this;
+            $("#useDan").on("click",function(){
+                _this._covert('\'');
+            });
+            $("#useShuang").on("click",function(){
+                _this._covert('"');
+            });
+        },
+        _covert:function(co){
+            var inStr = $("#in").val();
+            var sb = this._covert2js(inStr,co,this._isPlus());
+            $("#out").val(sb);
+        },
+        _covert2js:function(inStr,co,isPlus){
+            var ins = inStr.trim().replace(new RegExp(co,"g"),"\\"+co).split("\n");
+            if(isPlus){
+                var item = ins[0];
+                var sb = "";
+                for(var i=0;item;item=ins[++i]){
+                    sb += co+item+co+'+\n';
+                }
+                sb=sb.substr(0,sb.length-2);
+                sb+=';';
+                return sb;
+            }else{
+                var item = ins[0];
+                var sb = "[";
+                for(var i=0;item;item=ins[++i]){
+                    sb += co+item+co+',\n';
+                }
+                sb=sb.substr(0,sb.length-2);
+                sb+='].join("");';
+                return sb;
+            }
+
+        },
+        _isPlus:function(){
+            return $("#usePlus")[0].checked;
+        }
+    }
+    Html2js.create=function(){
+        var html2js = new Html2js();
+        html2js.init();
+        return html2js;
+    }
+
+    win.Html2js = Html2js;
+
+})(window);
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    Html2js.create();
+});
